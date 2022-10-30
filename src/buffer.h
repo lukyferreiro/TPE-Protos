8f33c8f01da37f74f0d76c545c257d3d58e90560
lukyferreiro <lferreiro@itba.edu.ar>
2022-10-30 20:52:33 -0300

    Add patches 1 to 5 and format code

--- src/buffer.h
+++ src/buffer.h
@@ -2,7 +2,7 @@
 #define BUFFER_H_VelRDAxzvnuFmwEaR0ftrkIinkT
 
 #include <stdbool.h>
-#include <unistd.h>  // size_t, ssize_t
+#include <unistd.h> // size_t, ssize_t
 
 /**
  * buffer.c - buffer con acceso directo (útil para I/O) que mantiene
@@ -83,67 +83,47 @@
  */
 typedef struct buffer buffer;
 struct buffer {
-    uint8_t *data;
+    uint8_t* data;
 
-    /** límite superior del buffer. inmutable */
-    uint8_t *limit;
+    /** Límite superior del buffer. inmutable */
+    uint8_t* limit;
 
-    /** puntero de lectura */
-    uint8_t *read;
+    /** Puntero de lectura */
+    uint8_t* read;
 
-    /** puntero de escritura */
-    uint8_t *write;
+    /** Puntero de escritura */
+    uint8_t* write;
 };
 
-/**
- * inicializa el buffer sin utilizar el heap
- */
-void
-buffer_init(buffer *b, const size_t n, uint8_t *data);
+/** Inicializa el buffer sin utilizar el heap */
+void buffer_init(buffer* b, const size_t n, uint8_t* data);
 
 /**
  * Retorna un puntero donde se pueden escribir hasta `*nbytes`.
  * Se debe notificar mediante la función `buffer_write_adv'
  */
-uint8_t *
-buffer_write_ptr(buffer *b, size_t *nbyte);
-void
-buffer_write_adv(buffer *b, const ssize_t bytes);
+uint8_t* buffer_write_ptr(buffer* b, size_t* nbyte);
+void buffer_write_adv(buffer* b, const ssize_t bytes);
 
-uint8_t *
-buffer_read_ptr(buffer *b, size_t *nbyte);
-void
-buffer_read_adv(buffer *b, const ssize_t bytes);
+uint8_t* buffer_read_ptr(buffer* b, size_t* nbyte);
+void buffer_read_adv(buffer* b, const ssize_t bytes);
 
-/**
- * obtiene un byte
- */
-uint8_t
-buffer_read(buffer *b);
+/** Obtiene un byte */
+uint8_t buffer_read(buffer* b);
 
-/** escribe un byte */
-void
-buffer_write(buffer *b, uint8_t c);
+/** Escribe un byte */
+void buffer_write(buffer* b, uint8_t c);
 
-/**
- * compacta el buffer
- */
-void
-buffer_compact(buffer *b);
+/** Compacta el buffer */
+void buffer_compact(buffer* b);
 
-/**
- * Reinicia todos los punteros
- */
-void
-buffer_reset(buffer *b);
+/** Reinicia todos los punteros */
+void buffer_reset(buffer* b);
 
-/** retorna true si hay bytes para leer del buffer */
-bool
-buffer_can_read(buffer *b);
+/** Retorna true si hay bytes para leer del buffer */
+bool buffer_can_read(buffer* b);
 
-/** retorna true si se pueden escribir bytes en el buffer */
-bool
-buffer_can_write(buffer *b);
-
+/** Retorna true si se pueden escribir bytes en el buffer */
+bool buffer_can_write(buffer* b);
 
 #endif
--- src/netutils_test.c
+++ src/netutils_test.c
@@ -1,24 +1,24 @@
+#include <check.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <check.h>
 
 #include "netutils.h"
 
-START_TEST (test_sockaddr_to_human_ipv4) {
+START_TEST(test_sockaddr_to_human_ipv4) {
     char buff[50] = {0};
 
     struct sockaddr_in addr = {
         .sin_family = AF_INET,
-        .sin_port   = htons(9090),
+        .sin_port = htons(9090),
     };
     addr.sin_addr.s_addr = htonl(0x01020304);
-    const struct sockaddr *x = (const struct sockaddr *) &addr;
+    const struct sockaddr* x = (const struct sockaddr*)&addr;
 
-    ck_assert_str_eq(sockaddr_to_human(buff, sizeof(buff)/sizeof(buff[0]), x),
+    ck_assert_str_eq(sockaddr_to_human(buff, sizeof(buff) / sizeof(buff[0]), x),
                      "1.2.3.4:9090");
-    ck_assert_str_eq(sockaddr_to_human(buff, 5,  x), "unkn");
-    ck_assert_str_eq(sockaddr_to_human(buff, 8,  x), "1.2.3.4");
-    ck_assert_str_eq(sockaddr_to_human(buff, 9,  x), "1.2.3.4:");
+    ck_assert_str_eq(sockaddr_to_human(buff, 5, x), "unkn");
+    ck_assert_str_eq(sockaddr_to_human(buff, 8, x), "1.2.3.4");
+    ck_assert_str_eq(sockaddr_to_human(buff, 9, x), "1.2.3.4:");
     ck_assert_str_eq(sockaddr_to_human(buff, 10, x), "1.2.3.4:9");
     ck_assert_str_eq(sockaddr_to_human(buff, 11, x), "1.2.3.4:90");
     ck_assert_str_eq(sockaddr_to_human(buff, 12, x), "1.2.3.4:909");
@@ -26,41 +26,39 @@ START_TEST (test_sockaddr_to_human_ipv4) {
 }
 END_TEST
 
-
-START_TEST (test_sockaddr_to_human_ipv6) {
+START_TEST(test_sockaddr_to_human_ipv6) {
     char buff[50] = {0};
 
     struct sockaddr_in6 addr = {
         .sin6_family = AF_INET6,
-        .sin6_port   = htons(9090),
+        .sin6_port = htons(9090),
     };
-    uint8_t *d = ((uint8_t *)&addr.sin6_addr);
-    for(int i = 0; i < 16; i++) {
+    uint8_t* d = ((uint8_t*)&addr.sin6_addr);
+    for (int i = 0; i < 16; i++) {
         d[i] = 0xFF;
     }
 
-    const struct sockaddr *x = (const struct sockaddr *) &addr;
+    const struct sockaddr* x = (const struct sockaddr*)&addr;
     ck_assert_str_eq(sockaddr_to_human(buff, 10, x), "unknown i");
     ck_assert_str_eq(sockaddr_to_human(buff, 39, x), "unknown ip:9090");
     ck_assert_str_eq(sockaddr_to_human(buff, 40, x),
-        "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff");
+                     "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff");
     ck_assert_str_eq(sockaddr_to_human(buff, 41, x),
-        "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff:");
+                     "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff:");
     ck_assert_str_eq(sockaddr_to_human(buff, 42, x),
-        "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff:9");
+                     "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff:9");
     ck_assert_str_eq(sockaddr_to_human(buff, 43, x),
-        "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff:90");
+                     "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff:90");
     ck_assert_str_eq(sockaddr_to_human(buff, 44, x),
-        "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff:909");
+                     "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff:909");
     ck_assert_str_eq(sockaddr_to_human(buff, 45, x),
-        "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff:9090");
+                     "ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff:9090");
 }
 END_TEST
 
-Suite * 
-hello_suite(void) {
-    Suite *s;
-    TCase *tc;
+Suite* hello_suite(void) {
+    Suite* s;
+    TCase* tc;
 
     s = suite_create("socks");
 
@@ -74,11 +72,10 @@ hello_suite(void) {
     return s;
 }
 
-int 
-main(void) {
+int main(void) {
     int number_failed;
-    Suite *s;
-    SRunner *sr;
+    Suite* s;
+    SRunner* sr;
 
     s = hello_suite();
     sr = srunner_create(s);
@@ -88,4 +85,3 @@ main(void) {
     srunner_free(sr);
     return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
-
